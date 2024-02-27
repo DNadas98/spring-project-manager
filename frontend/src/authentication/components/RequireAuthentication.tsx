9989d73caf60d50c910d1867b29c7668b09c7552
DNadas98 <daniel.nadas.123@gmail.com>
2024-02-27 10:51:38 +0100

    refactor refresh logic on the frontend

--- frontend/src/authentication/components/RequireAuthentication.tsx
+++ frontend/src/authentication/components/RequireAuthentication.tsx
@@ -6,50 +6,66 @@ import {useEffect, useState} from "react";
 import useRefresh from "../hooks/useRefresh.ts";
 import LoadingSpinner from "../../common/utils/components/LoadingSpinner.tsx";
 import {useAuthentication} from "../hooks/useAuthentication.ts";
-import {useNotification} from "../../common/notification/context/NotificationProvider.tsx";
+import {
+  useNotification
+} from "../../common/notification/context/NotificationProvider.tsx";
 
 interface RequireAuthProps {
-    allowedRoles: Array<GlobalRole>;
+  allowedRoles: Array<GlobalRole>;
 }
 
 export default function RequireAuthentication({allowedRoles}: RequireAuthProps) {
-    const [loading, setLoading] = useState(true);
-    const [allowed, setAllowed] = useState(false);
-    const authentication = useAuthentication();
-    const notification = useNotification();
-    const refresh = useRefresh();
-    const logout = useLogout();
+  const [loading, setLoading] = useState(true);
+  const [allowed, setAllowed] = useState(false);
+  const authentication = useAuthentication();
+  const notification = useNotification();
+  const refresh = useRefresh();
+  const logout = useLogout();
 
-    useEffect(() => {
-        if (!authentication.getAccessToken()?.length) {
-            refresh().then();
+  async function handleUnauthorized() {
+    notification.openNotification({
+      type: "error", vertical: "top", horizontal: "center",
+      message: "Unauthorized"
+    });
+    await logout();
+  }
+
+  async function handleAccessDenied() {
+    notification.openNotification({
+      type: "error", vertical: "top", horizontal: "center",
+      message: "Access Denied"
+    });
+    await logout();
+  }
+
+  useEffect(() => {
+    async function verifyAllowed() {
+      let roles = authentication.getRoles();
+      if (!authentication.getAccessToken()?.length) {
+        const refreshResponseDto = await refresh();
+        const refreshedRoles = refreshResponseDto?.newAuthentication?.userInfo?.roles;
+        if (refreshedRoles?.length) {
+          roles = refreshedRoles;
+        } else {
+          return await handleUnauthorized();
         }
-    }, []);
-
-    useEffect(() => {
-        async function verifyAllowed() {
-            const roles = authentication.getRoles();
-            if (roles?.some(role => allowedRoles.includes(role))) {
-                setAllowed(true);
-            } else {
-                notification.openNotification({
-                    type: "error", message: "Unauthorized", vertical: "top", horizontal: "center"
-                });
-                await logout();
-            }
-        }
-
-        if (authentication.getAccessToken()?.length) {
-            verifyAllowed().finally(() => {
-                setLoading(false);
-            });
-        }
-    }, [authentication]);
-
-    if (loading) {
-        return (<LoadingSpinner/>);
-    } else if (allowed) {
-        return (<Outlet/>);
+      }
+      if (roles?.some(role => allowedRoles.includes(role))) {
+        setAllowed(true);
+      } else {
+        return await handleAccessDenied();
+      }
     }
-    return null;
+
+    verifyAllowed().finally(() => {
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return (<LoadingSpinner/>);
+  } else if (allowed) {
+    return (<Outlet/>);
+  }
+  return null;
 }
